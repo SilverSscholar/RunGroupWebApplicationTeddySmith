16fc9775fe47e27b8d956ddb822984cb9b5acd02
SilverSscholar <silversscholar@gmail.com>
2022-06-03 19:09:03 -0400

    Models

---
+++ Data/Enum/ClubCategory.cs
@@ -0,0 +1,11 @@
+﻿namespace RunGroupWebApplicationTeddySmith.Data.Enum
+{
+    public enum ClubCategory
+    {
+        RoadRunner,
+        Womens,
+        City,
+        Trail,
+        Endurance
+    }
+}
---
+++ Data/Enum/RaceCategory.cs
@@ -0,0 +1,11 @@
+﻿namespace RunGroupWebApplicationTeddySmith.Data.Enum
+{
+    public enum RaceCategory
+    {
+        Marathon,
+        Ultra,
+        FiveK,
+        TenK,
+        HalfMarathon
+    }
+}
